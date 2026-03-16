dd29930a74856f0d876b1495cdcc2909a6a8646d
Rishabh Kumar Bhatnagar <rishabhkumarbhatnagar@gmail.com>
2026-03-16 21:03:57 +0530

    add container

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ resource "azurerm_resource_group" "rg" {
   location = "eastus"
 }
 
-# resource "azurerm_storage_container" "container" {
-#     name = "tf.state"
-#     container_access_type = "private"
-# }
+resource "azurerm_storage_container" "container" {
+    name = "tf.state"
+    container_access_type = "private"
+}
 
 # resource "azurerm_resource_group" "change1" {
 #   name     = "rg-change"
