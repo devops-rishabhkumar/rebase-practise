4af003188af3fd68d30ea4cc42d10498fe6d001a
Rishabh Kumar Bhatnagar <rishabhkumarbhatnagar@gmail.com>
2026-03-16 21:08:37 +0530

    add 3rd container

--- main.tf
+++ main.tf
@@ -21,3 +21,8 @@ resource "azurerm_storage_container" "container2" {
     name = "tf.state"
     container_access_type = "public"
 }
+
+resource "azurerm_storage_container" "container3" {
+    name = "tf.state"
+    container_access_type = "public"
+}
